c3ade0bd67d7aad0aa210261fa4a2e109016ebbf
plymatea <andy.plymate@gmail.com>
2022-03-04 11:57:00 -0800

    show bad fail on VendorConstructor_retrieve Test.

--- Vendors.Tests/ModelTests/VendorsTest.cs
+++ Vendors.Tests/ModelTests/VendorsTest.cs
@@ -6,19 +6,29 @@ using System;
 namespace Vendors.Tests
 {
   [TestClass]
-  public class VendorTests /*: IDisposable*/
+  public class VendorTests : IDisposable
   {
 
-  //   public void Dispose()
-  //   {
-  //     Vendor.ClearAll();
-  //   }
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
 
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfItem_Item()
     {
       Vendor newVendor = new Vendor("test");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+    [TestMethod]
+    public void VendorConstructior_RetrievePropertiesOfItem_True()
+    {
+      Vendor newVendor = new Vendor("test");
+      Assert.AreEqual("hello", newVendor.Name);
+      // Assert.AreEqual(1, newVendor.Id);
+      // List<Order> orders = new List<Order> {};
+      // CollectionAssert.AreEqual(orders, newVendor.Orders);
     }
   }
 }
