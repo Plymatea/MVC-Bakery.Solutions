cf209dc093f18552a608097e6fad657136e79126
plymatea <andy.plymate@gmail.com>
2022-03-04 14:30:58 -0800

    show order creation and display funtionality works.

--- Vendors/Controllers/OrderController.cs
+++ Vendors/Controllers/OrderController.cs
@@ -19,7 +19,7 @@ namespace Vendors.Controllers
       Order order = Order.Find(orderId);
       Vendor vendor = Vendor.Find(vendorId);
       Dictionary<string, object> model = new Dictionary<string, object>();
-      model.Add("order", order);
+      model.Add("orders", order);
       model.Add("vendors", vendor);
       return View(model);
     }
